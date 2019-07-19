64037dd9d32466632dd619d6d06041ec2884b919
Marisa Ramon <mer0522@gmail.com>
2019-07-19 09:44:22 -0500

    add static route so that css file would load

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,10 +10,14 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
     // A default, catch-all route that leads to home.html which displays the home page.
-    app.use(function(req,res) {
+    // app.use(function(req,res) {
+    //     res.sendFile(path.join(__dirname + "/../public/home.html"));
+    // });
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 };
+
 
 
 
--- server.js
+++ server.js
@@ -10,12 +10,14 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // ROUTES
 require("./app/routing/apiRoutes.js")(app);
 require ("./app/routing/htmlRoutes.js")(app);
+app.use(express.static("./app/public"));
 
 // LISTENER
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: http://localhost:" + PORT);
   });
   
